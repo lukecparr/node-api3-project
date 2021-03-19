62a2e839a8e7c8b1d5b7748cb798462d8b71b7de
Lukecp <lukecp@pm.me>
2021-03-18 22:04:03 -0400

    converted Port to env variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// require your server and launch it
+require('dotenv').config();
 const server = require('./api/server');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-server.listen(port, () => {console.log(`Server listening on port ${port}...`)})
+server.listen(port, () => {
+	console.log(`Server listening on port ${port}...`);
+});
